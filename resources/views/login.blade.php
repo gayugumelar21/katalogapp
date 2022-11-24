b41c1103404cea078ad3768ae45f03e3570876b0
Gayu Gumelar <72981058+gayugumelar21@users.noreply.github.com>
2022-11-24 10:44:26 +0700

    Perbaikan halaman login
    Perbaikan halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,7 @@
     <title>KatalogApp</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/berandadepan.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
 </head>
 <body>
     <header>
@@ -22,28 +23,26 @@
 
     <!-- Tentang -->
 	<div class="section">
-		<div class="container-login">
-			<div class="login">
-          <form action="{{ route('login') }}" method="post" class="form-signin">
-            @csrf
-            @if (Session::has('error'))
-                <div>
-                    {{ Session::get('error') }}
+            <div class="box-login">
+                <div class="form-login">
+                    <form action="{{ route('login') }}" method="post" class="form-signin">
+                        @csrf
+                        @if (Session::has('error'))
+                            <div>
+                                {{ Session::get('error') }}
+                            </div>
+                        @endif
+                        <!-- <label for="">Masukan Email</label> -->
+                        <input type="email" id="email" name="email" placeholder="Masukan Email" value="admin@gmail.com" class="email" required autofocus>
+                        
+                        <!-- <label for="">Masukan Password</label> -->
+                        <input type="password" id="password" name="password" placeholder="Masukan Password" value="admin321" class="password" required>
+
+                        <button type="submit" class="btn">Sign In</button>
+                    </form>
                 </div>
-            @endif
-            <div>
-              <label for="inputEmail">Masukan Email</label>
-              <input type="email" id="email" name="email" placeholder="Masukan Email" value="admin@gmail.com" required autofocus>
-            </div>
-            <div>
-              <label for="inputPassword">Masukan Password</label>
-              <input type="password" id="password" name="password" placeholder="Masukan Password" value="admin321" required>
-            </div>
-            <button type="submit">Login</button>
-        </form>
-			</div>
 		</div>
 	</div>
-
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
 </body>
 </html>
